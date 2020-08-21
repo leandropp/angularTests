927f20b4301543e259d0435e6247662baac68d6b
Leandro Pereira da Paixão <leandro.paixao@marttech.com.br>
2020-08-20 23:24:29 -0300

    readequação do layout, com uso de único painel, somente login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './Login/login.component.html',
+  styleUrls: ['./Login/login.component.css']
 })
 export class AppComponent {
   title = 'firstProjectAngular';
